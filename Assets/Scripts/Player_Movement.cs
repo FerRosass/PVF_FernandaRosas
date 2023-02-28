9f224d2b07948d7b5b4a1984eb29ff572969e893
FerRosass <104027833+FerRosass@users.noreply.github.com>
2023-02-28 17:39:08 -0600

    giraaaaa

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float speed = 5.0f;
+    /*public float speed = 5.0f;
     public float xRange = 5.0f;
 
     private float horizontalInput;
@@ -12,7 +12,8 @@ public class Player_Movement : MonoBehaviour
     public Logic_Script logic;
     public bool FarmerAlive = true;
     private Animator animator;
-    bool rightmovement = true;
+    private bool rightmovement = true;
+    public bool isFacingRight = true;
 
     void Start()
     {
@@ -24,6 +25,18 @@ public class Player_Movement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        animator.SetBool("isMoving", horizontalInput != 0f);
+
+        if (horizontalInput > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (horizontalInput < 0 && isFacingRight)
+        {
+            Flip();
+        }
+
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -32,18 +45,57 @@ public class Player_Movement : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        animator.SetFloat("Horizontal", Mathf.Abs(horizontalInput));
-        Direction(horizontalInput);
     }
-    void Direction(float horizontalInput)
+    void Flip()
     {
-        if(rightmovement= true && horizontalInput<0)
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}*/
+    // La velocidad de movimiento del jugador
+    [SerializeField] private float movementSpeed = 5f;
+    // Determina si el jugador está mirando hacia la derecha
+    private bool isFacingRight = true;
+    // El componente Rigidbody2D del jugador
+    private Rigidbody2D rb;
+    // El componente Animator del jugador
+    private Animator anim;
+
+    void Start()
+    {
+        // Obtiene el componente Rigidbody2D del jugador
+        rb = GetComponent<Rigidbody2D>();
+        // Obtiene el componente Animator del jugador
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        // Obtiene el valor dado por el input
+        float moveInput = Input.GetAxis("Horizontal");
+        // Mueve el jugador en la dirección horizontal deseada
+        rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+
+        // Actualiza la animación de caminar del jugador
+        anim.SetBool("IsMoving", moveInput != 0f);
+
+        // Si el jugador se mueve hacia la derecha y no está mirando hacia la derecha, voltea el sprite del jugador
+        if (moveInput > 0 && !isFacingRight)
         {
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            Flip();
         }
-        else if(rightmovement = false && horizontalInput > 0)
+        // Si el jugador se mueve hacia la izquierda y está mirando hacia la derecha, voltea el sprite del jugador
+        else if (moveInput < 0 && isFacingRight)
         {
-            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            Flip();
         }
+    }
+
+    // Voltea el sprite del jugador horizontalmente
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
+
